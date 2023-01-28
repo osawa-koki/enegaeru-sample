ceca9644c0dc5d0dd3a25948443b54bbedf7806f
osawa-koki <github@osawa.dev>
2023-01-28 13:15:23 +0900

    設定値イロイロ。

---
+++ interface/SharedData.ts
@@ -0,0 +1,8 @@
+
+type SharedData = {
+  api_key: string;
+  username: string;
+  password: string;
+};
+
+export default SharedData;
--- pages/setting.tsx
+++ pages/setting.tsx
@@ -3,10 +3,16 @@ import { DataContext } from '../components/DataContext';
 import { Button, Alert, Form } from 'react-bootstrap';
 
 import Layout from "../components/Layout";
+import SharedData from '../interface/SharedData';
 
 export default function Setting() {
 
-  const { sharedData, setSharedData } = useContext(DataContext);
+  const { sharedData, setSharedData } = useContext<
+    {
+      sharedData: SharedData;
+      setSharedData: React.Dispatch<React.SetStateAction<SharedData>>;
+    }
+  >(DataContext);
 
   const SetApiKey = (e: any) => {
     const input = e.target as HTMLInputElement;
@@ -36,9 +42,16 @@ export default function Setting() {
         <Form.Group className="mt-3">
           <Form.Label>Enter API key</Form.Label>
           <Form.Control type="text" placeholder="Enter API_KEY" value={sharedData.api_key} onInput={SetApiKey} />
-          <Form.Text>
-            We'll never share your api_key with anyone else.
-          </Form.Text>
+        </Form.Group>
+        <hr />
+        <Form.Group className="mt-3">
+          <Form.Label>Enter Username</Form.Label>
+          <Form.Control type="text" placeholder="Enter Username" value={sharedData.username} onInput={SetUsername} />
+        </Form.Group>
+        <hr />
+        <Form.Group className="mt-3">
+          <Form.Label>Enter Password</Form.Label>
+          <Form.Control type="text" placeholder="Enter Password" value={sharedData.password} onInput={SetPassword} />
         </Form.Group>
       </div>
     </Layout>
